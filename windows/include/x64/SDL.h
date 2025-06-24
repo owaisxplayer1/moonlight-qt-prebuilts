b76cf108ec13310364b8bc849661959071d10d9b
owaisxplayer1 <sh.owais66@gmail.com>
2025-06-24 12:29:01 +0530

    Add macro to disable Oneplay QT Logger in Third party SDL library; This is bad coding but whatever

--- windows/include/x64/SDL.h
+++ windows/include/x64/SDL.h
@@ -63,9 +63,11 @@
 #include "SDL_locale.h"
 #include "SDL_misc.h"
 
+#ifndef NOTQT
 //Included Our Logger header. 
 // So any file that calls SDL logs can call our sdl logging macro which also logs function name and file name info
 #include "OP_SDL_LOG_HEADER.h"
+#endif
 
 #include "begin_code.h"
 /* Set up for C function definitions, even when using C++ */
